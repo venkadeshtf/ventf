fdff2d0563e5b56b31f0bac900af7a97c619f8bb
venkadeshtf <115581084+venkadeshtf@users.noreply.github.com>
2022-10-11 23:33:07 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "rg_name" {
+  default = "RG-DEEP-001"
+}
+
+variable "rg_name2" {
+  default = "RG-MURALI-002"
+}
+
+variable "rglocation" {
+  default = "West Europe"
+}
+
+variable "storageacname2966" {
+  default = "uatopsstorageac45690"
+}
+
+variable "rg_name67" {
+  default = "RG-VENKAT-002"
+}
+
+variable "rg_name99" {
+  default = "RG-VENKATTERFF01"
+}
