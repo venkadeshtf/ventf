91670ecda96bc5440c8b918fa508f7c46cadf5d2
venkadeshtf <115581084+venkadeshtf@users.noreply.github.com>
2022-10-12 00:18:04 +0530

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -24,11 +24,11 @@ resource "azurerm_storage_account" "rg-st1" {
   location                 = azurerm_resource_group.rg-tf1.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-}
 
   tags = {
     environment = "staging"
   }
+} 
 
 resource "azurerm_storage_account" "rg-devopsst" {
   name                     = var.storageacname12102022
