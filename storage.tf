ce7416893aef11cd9bb2fa57e736ff8f0b4b3adb
venkadeshtf <115581084+venkadeshtf@users.noreply.github.com>
2022-10-14 23:48:07 +0530

    Add files via upload

---
+++ storage.tf
@@ -0,0 +1,50 @@
+resource "azurerm_resource_group" "rg-deepak" {
+  name     = var.rgdeepak1
+  location = var.rgdeepaklocation
+}
+
+resource "azurerm_resource_group" "rg-murali" {
+  name     = var.rgmurali
+  location = var.rgmuralilocation
+}
+
+resource "azurerm_resource_group" "rg-venkat" {
+  name     = var.rgvenkat
+  location = var.rgvenkatlocation
+}
+
+resource "azurerm_storage_account" "rg-dks111" {
+  name                     = var.eeffgg
+  resource_group_name      = azurerm_resource_group.rg-deepak.name
+  location                 = azurerm_resource_group.rg-deepak.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+} 
+
+resource "azurerm_storage_account" "rg-mks222" {
+  name                     = var.aabbcc
+  resource_group_name      = azurerm_resource_group.rg-murali.name
+  location                 = azurerm_resource_group.rg-murali.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_account" "rg-vps333" {
+  name                     = var.qqwwee
+  resource_group_name      = azurerm_resource_group.rg-venkat.name
+  location                 = azurerm_resource_group.rg-venkat.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+} 
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "rgdeepak1" {
+  default = "RG-DEEP-2966"
+}
+
+variable "rgmurali" {
+  default = "RG-MURALI-2555"
+}
+
+variable "rgvenkat" {
+  default = "RG-venkat-11"
+}
+
+variable "rgdeepaklocation" {
+  default = "West Europe"
+}
+
+variable "rgmuralilocation" {
+  default = "West Europe"
+}
+
+variable "rgvenkatlocation" {
+  default = "West Europe"
+}
+
+variable "eeffgg" {
+  default = "trich"
+}
+
+variable "aabbcc" {
+  default = "pall"
+}
+
+variable "qqwwee" {
+  default = "chrom"
+}
