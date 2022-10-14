303d1608ff40fbca321e02dac7f1ad64b678b292
venkadeshtf <115581084+venkadeshtf@users.noreply.github.com>
2022-10-15 01:34:21 +0530

    Add files via upload

---
+++ storage.tf
@@ -0,0 +1,50 @@
+resource "azurerm_resource_group" "zzd" {
+  name     = var.azz199
+  location = var.clocd
+}
+
+resource "azurerm_resource_group" "yym" {
+  name     = var.byy299
+  location = var.dlocm
+}
+
+resource "azurerm_resource_group" "xxv" {
+  name     = var.cxx399
+  location = var.elocv
+}
+
+resource "azurerm_storage_account" "savd" {
+  name                     = var.mi600
+  resource_group_name      = azurerm_resource_group.zzd.name
+  location                 = azurerm_resource_group.zzd.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+} 
+
+resource "azurerm_storage_account" "zanm" {
+  name                     = var.xio711
+  resource_group_name      = azurerm_resource_group.yym.name
+  location                 = azurerm_resource_group.yym.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_account" "bub" {
+  name                     = var.sung888
+  resource_group_name      = azurerm_resource_group.bub.name
+  location                 = azurerm_resource_group.bub.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+} 
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "azz199" {
+  default = "crnewkapeed"
+}
+
+variable "byy299" {
+  default = "secalimur"
+}
+
+variable "cxx399" {
+  default = "resvenhesak"
+}
+
+variable "clocd" {
+  default = "West Europe"
+}
+
+variable "dlocm" {
+  default = "West Europe"
+}
+
+variable "elocv" {
+  default = "West Europe"
+}
+
+variable "mi600" {
+  default = "mrmyes"
+}
+
+variable "xio711" {
+  default = "yremes"
+}
+
+variable "sung888" {
+  default = "asdfg"
+}
